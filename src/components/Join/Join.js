78433ca143ed9ba2d9f379e08671f877767a24a7
ramon_ramirez <marluxia122@hotmail.com>
2020-04-15 17:46:16 -0600

    first git hub commit

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -13,8 +13,8 @@ const Join= () => {
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
                 <h1 className="heading">Join</h1>
-                <div><input    plaveholder="Name" className="joinInput" type="text" onChange={(event)=> setName( event.target.value)}/>  </div>
-                <div><input    plaveholder="Room" className="joinInput mt-20" type="text" onChange={(event)=> setRoom( event.target.value)}/>  </div>
+                <div><input    placeholder="Name" className="joinInput" type="text" onChange={(event)=> setName( event.target.value)}/>  </div>
+                <div><input    placeholder="Room" className="joinInput mt-20" type="text" onChange={(event)=> setRoom( event.target.value)}/>  </div>
                 <Link onClick={event =>(!name || !room) ? event.preventDefault() : null } to={`/chat?name=${name}&room=${room}`}>
                       <button className="button mt-20" type="submit">Sing In</button>
                 
